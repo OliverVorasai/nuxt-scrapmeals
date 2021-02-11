e46cf4c3030d0097b6ad4bfac5b9c02d6def37c7
olivervorasai <olivervorasai@gmail.com>
2021-02-10 16:12:07 -0800

    Add 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,25 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
+  <v-row justify="center">
+    <v-col v-if="error.statusCode === 404" cols="12">
+      <v-card max-width="400" class="pa-6 mx-auto">
+        <v-img
+          :src="require('@/assets/svg/error-404-colour.svg')"
+          max-height="400"
+          max-width="400"
+          contain
+        ></v-img>
+        <v-card-text class="test--primary text-h6">
+          {{ pageNotFound }}
+        </v-card-text>
+        <v-card-actions>
+          <v-btn class="mx-auto" color="primary" to="/" nuxt>Return Home</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col v-else cols="12">
       {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </v-app>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -21,7 +33,7 @@ export default {
   },
   data() {
     return {
-      pageNotFound: '404 Not Found',
+      pageNotFound: "We're sorry, that page does not seem to exist.",
       otherError: 'An error occurred',
     }
   },
