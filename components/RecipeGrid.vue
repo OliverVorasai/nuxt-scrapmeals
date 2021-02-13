e894d528c232e91389f88153b670cddfa5902d8a
olivervorasai <olivervorasai@gmail.com>
2021-02-12 23:08:00 -0800

    Recipe Cards now slide in on hydration

--- components/RecipeGrid.vue
+++ components/RecipeGrid.vue
@@ -1,29 +1,35 @@
 <template>
-  <v-row justify="center">
+  <div>
     <!-- Grid for Recipe Cards -->
-    <template v-for="(recipe, index) in recipeList">
+    <transition-group appear name="slide-in" tag="v-row" class="justify-center">
       <recipe-card
+        v-for="(recipe, index) in recipeList"
         :key="recipe.id + `index${index}`"
         :recipe="recipe"
+        :style="{ '--i': index % 10 }"
       ></recipe-card>
-    </template>
+    </transition-group>
     <!-- Button to show more recipes -->
-    <v-col v-if="recipes.length > 0" cols="12" class="text-center">
-      <v-btn
-        v-show="displayCount < recipes.length"
-        color="info"
-        @click="loadMore"
-      >
-        See More Recipes
-      </v-btn>
-    </v-col>
+    <v-row>
+      <v-col v-if="recipes.length > 0" cols="12" class="text-center">
+        <v-btn
+          v-show="displayCount < recipes.length"
+          color="info"
+          @click="loadMore"
+        >
+          See More Recipes
+        </v-btn>
+      </v-col>
+    </v-row>
     <!-- Plain card for no results -->
-    <v-col v-if="showNoResults" cols="12" sm="8" md="6" lg="4">
-      <v-card class="mx-auto">
-        <v-card-title class="justify-center"> No Results Found </v-card-title>
-      </v-card>
-    </v-col>
-  </v-row>
+    <v-row>
+      <v-col v-if="showNoResults" cols="12" sm="8" md="6" lg="4">
+        <v-card class="mx-auto">
+          <v-card-title class="justify-center"> No Results Found </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -65,3 +71,35 @@ export default {
   },
 }
 </script>
+
+<style scoped lang="scss">
+.slide-in {
+  &-move {
+    transition: opacity 0.5s linear, transform 0.5s ease-in-out;
+  }
+
+  &-leave-active {
+    transition: opacity 0.4s linear,
+      transform 0.4s cubic-bezier(0.5, 0, 0.7, 0.4);
+    transition-delay: calc(0.1s * (var(10) - var(--i)));
+  }
+
+  &-enter-active {
+    transition: opacity 0.5s linear,
+      transform 0.5s cubic-bezier(0.2, 0.5, 0.1, 1);
+    transition-delay: calc(0.5s + (0.1s * var(--i)));
+  }
+
+  &-enter,
+  &-leave-to {
+    opacity: 0;
+  }
+
+  &-enter {
+    transform: translateX(-1em);
+  }
+  &-leave-to {
+    transform: translateX(1em);
+  }
+}
+</style>
